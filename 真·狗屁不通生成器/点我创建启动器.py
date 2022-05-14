7014cde34a62c1e931ce2ae90cb61eb0191ad7c9
Fang Yiheng <100507147+KINGXGM@users.noreply.github.com>
2022-05-14 18:53:26 +0800

    Update 点我创建启动器.py

--- 真·狗屁不通生成器/点我创建启动器.py
+++ 真·狗屁不通生成器/点我创建启动器.py
@@ -3,11 +3,13 @@ import os
 path = os.getcwd()
 
 with open("点我创建神经网络生成器.bat","w",encoding="utf-8") as file:
+file.write("@echo off\necho [32m\n")
     file.write(path[0:2]+"\n")
-    file.write("python main.py\n")
+    file.write("python main.py\necho [0m\n")
     file.write("pause")
 with open("点我测试神经网络.bat","w",encoding="utf-8") as file:
+    file.write("@echo off\necho [32m\n")
     file.write(path[0:2]+"\n")
-    file.write("python main2.py")
+    file.write("python main2.py\necho [0m")
 
 print("启动器修改完毕>>>")
